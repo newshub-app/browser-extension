33513883ff319dced87cbce48053c68de666830e
Mathieu Deous <mat.deous@gmail.com>
2024-07-14 15:01:58 +0200

    refactor content script

--- content.tsx
+++ content.tsx
@@ -3,18 +3,18 @@ import {useMessage} from "@plasmohq/messaging/dist/hook"
 const UNKNOWN = "unknown"
 
 interface PageInfo {
-    title: string,
     url: string,
+    title?: string,
     description?: string
 }
 
 const ContentScriptHandler = () => {
     useMessage<string, PageInfo>(async (req, res) => {
         if (req.name === "pageinfo") {
-            const title = document.title ?? UNKNOWN
-            const url = window.location.href ?? UNKNOWN
-            const description = document.querySelector("meta[name='description']")?.getAttribute("content") ?? UNKNOWN
-            res.send({title: title, url: url, description: description})
+            const url = window.location.href ?? ""
+            const title = document.title ?? ""
+            const description = document.querySelector("meta[name='description']")?.getAttribute("content") ?? ""
+            res.send({url, title, description})
         }
     })
 }
