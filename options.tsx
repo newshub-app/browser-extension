e41caef014f407008b41f18fcd13cde84c1ea2f2
Mathieu Deous <mat.deous@gmail.com>
2024-07-14 19:06:33 +0200

    working options page

--- options.tsx
+++ options.tsx
@@ -6,22 +6,28 @@ import Button from "react-bootstrap/Button";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "~styles.css";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
 
-interface FormData {
-    url: string
-    token: string
+interface Settings {
+    api_url: string
+    api_token: string
 }
 
 const OptionsPage = () => {
     const [formValid, setFormValid] = useState<boolean>(false);
-    const [formData, setFormData] = useState<FormData>({url: "", token: ""});
-    const [_url, setUrl] = useStorage<string>("newshubUrl", "");
-    const [_token, setToken] = useStorage<string>("apiToken", "");
+    const [settings, _setsettings, {
+        setRenderValue,
+        setStoreValue
+    }] = useStorage<Settings>(
+        "settings",
+        {"api_url": "", "api_token": ""}
+    );
 
     const handleChange = (e) => {
         const {name, value} = e.currentTarget;
-        setFormData({
-            ...formData,
+        console.log(name, value);
+        setRenderValue({
+            ...settings,
             [name]: value,
         });
     }
@@ -32,6 +38,7 @@ const OptionsPage = () => {
             e.preventDefault()
             e.stopPropagation()
         }
+        setStoreValue(settings).catch(err => {console.error(err)})
         setFormValid(true)
         e.preventDefault()
     }
@@ -39,28 +46,34 @@ const OptionsPage = () => {
     return (
         <Container>
             <h1>NewsHub Extension Settings</h1>
-            <Form noValidate validated={formValid} onSubmit={handleSubmit}>
+            <Form validated={formValid} onSubmit={handleSubmit}>
                 <Form.Group controlId="newshubUrl">
-                    <Form.Label>NewsHub URL</Form.Label>
-                    <Form.Control name="url"
-                                  type="url"
-                                  value={formData.url}
-                                  onChange={handleChange}
-                                  required
-                    />
+                    <FloatingLabel label="NewsHub instance API URL"
+                                   controlId="newshubUrl"
+                    >
+                        <Form.Control name="api_url"
+                                      type="url"
+                                      value={settings.api_url}
+                                      onChange={handleChange}
+                                      required
+                        />
+                    </FloatingLabel>
                     <Form.Control.Feedback type="invalid">
                         Please enter a valid URL
                     </Form.Control.Feedback>
                 </Form.Group>
                 <Form.Group controlId="apiToken">
-                    <Form.Label>API Token</Form.Label>
-                    <Form.Control name="token"
-                                  type="text"
-                                  pattern="[a-f0-9]{32}"
-                                  value={formData.token}
-                                  onChange={handleChange}
-                                  required
-                    />
+                    <FloatingLabel label="API Token"
+                                   controlId="newshubToken"
+                    >
+                        <Form.Control name="api_token"
+                                      type="text"
+                                      pattern="[a-f0-9]{32}"
+                                      value={settings.api_token}
+                                      onChange={handleChange}
+                                      required
+                        />
+                    </FloatingLabel>
                     <Form.Control.Feedback type="invalid">
                         Please enter a valid API token
                     </Form.Control.Feedback>
