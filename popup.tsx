4e44b4c8df2d2ad40f50d8a4c34d777fa23968d9
Mathieu Deous <mat.deous@gmail.com>
2024-07-14 16:04:56 +0200

    show settings button on top + use floating labels

--- popup.tsx
+++ popup.tsx
@@ -3,6 +3,8 @@ import {sendToContentScript} from "@plasmohq/messaging"
 import Button from "react-bootstrap/Button"
 import Container from "react-bootstrap/Container"
 import Form from "react-bootstrap/Form"
+import FloatingLabel from "react-bootstrap/FloatingLabel"
+import Stack from "react-bootstrap/Stack"
 import {Gear} from "react-bootstrap-icons"
 
 import "bootstrap/dist/css/bootstrap.css"
@@ -27,32 +29,42 @@ function IndexPopup() {
 
     return (
         <Container>
-            <h3>Add to NewsHub</h3>
+            <Stack direction="horizontal">
+                <div>
+                    <h3>Add to NewsHub</h3>
+                </div>
+                <div className="ms-auto">
+                    <a href="/options.html" target="_blank" title="Settings"><Gear/></a>
+                </div>
+            </Stack>
             <Form>
                 <Form.Group controlId="urlField">
-                    <Form.Label>URL</Form.Label>
-                    <Form.Control type="text"
+                    <FloatingLabel label="URL">
+                        <Form.Control type="text"
                                   onChange={(e) => setPageUrl(e.target.value)}
                                   value={pageUrl}
-                    />
+                        />
+                    </FloatingLabel>
                 </Form.Group>
                 <Form.Group controlId="titleField">
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control type="text"
-                                  onChange={(e) => setPageTitle(e.target.value)}
-                                  value={pageTitle}
-                    />
+                    <FloatingLabel label="Title">
+                        <Form.Control type="text"
+                                      onChange={(e) => setPageTitle(e.target.value)}
+                                      value={pageTitle}
+                        />
+                    </FloatingLabel>
                 </Form.Group>
                 <Form.Group controlId="descField">
-                    <Form.Label>Description</Form.Label>s
-                    <Form.Control type="text"
-                                  onChange={(e) => setPageDesc(e.target.value)}
-                                  value={pageDesc}
-                    />
+                    <FloatingLabel label="Description">
+                        <Form.Control as="textarea"
+                                      onChange={(e) => setPageDesc(e.target.value)}
+                                      value={pageDesc}
+                                      style={{height: "8em"}}
+                        />
+                    </FloatingLabel>
                 </Form.Group>
-                <Button className="submitBtn" type="submit">Save to NewsHub</Button>
+                <Button className="submitBtn" type="submit">Add link</Button>
             </Form>
-            <a href="/options.html" target="_blank"><Gear/></a>
         </Container>
     )
 }
